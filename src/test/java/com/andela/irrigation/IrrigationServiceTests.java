6a266b433e31a192ebff09785ceccd0ec37977cb
Mina <mina.aboalim@gmail.com>
2022-11-20 03:03:52 +0100

    write using TDD the main test cases which should be covered

---
+++ src/test/java/com/andela/irrigation/IrrigationServiceTests.java
@@ -0,0 +1,41 @@
+package com.andela.irrigation;
+
+import org.junit.jupiter.api.Test;
+
+public class IrrigationServiceTests {
+
+    @Test
+    void getIrrigationById_NewIrrigation_NotFound() {
+
+    }
+
+    @Test
+    void getIrrigationById_ExistingIrrigation_Found() {
+
+    }
+
+    @Test
+    void registerIrrigationForLand_InvalidData_ValidationError() {
+
+    }
+
+    @Test
+    void registerIrrigationForLand_MissingMandatoryData_ValidationError() {
+
+    }
+
+    @Test
+    void registerIrrigationForLand_ValidData_Success() {
+
+    }
+
+    @Test
+    void updateIrrigationForLand_InvalidData_ValidationError() {
+
+    }
+
+    @Test
+    void updateIrrigationForLand_ValidData_Success() {
+
+    }
+}
